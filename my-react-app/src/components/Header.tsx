3098ec9c7aa5f9dc64c46bc8f67560ef60d25d2b
novakvova <novakvova@gmail>
2025-03-21 19:26:02 +0200

    Is good working app login google completed

--- my-react-app/src/components/Header.tsx
+++ my-react-app/src/components/Header.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+import {logOut} from "../store/slices/userSlice.ts";
+import {useAppDispatch, useAppSelector} from "../store";
+import {useNavigate} from "react-router-dom";
+
 const Header: React.FC = () => {
+
+    const dispatcher = useAppDispatch();
+    const {user} = useAppSelector((state) => state.user);
+    const navigate = useNavigate();
+
     return (
         <header className="bg-white shadow p-4 flex justify-between items-center">
             <h1 className="text-xl font-bold text-gray-800">My Website</h1>
@@ -27,18 +36,40 @@ const Header: React.FC = () => {
                             Contact
                         </a>
                     </li>
+                    {user ?
+                        <>
+                            <li>
+                                <Link to="/profile" className="text-gray-600 hover:text-blue-500">
+                                    {user.email}
+                                </Link>
+                            </li>
 
-                    <li>
-                        <Link to="/register" className="text-gray-600 hover:text-blue-500">
-                            Реєстрація
-                        </Link>
-                    </li>
+                            <li>
+                                <a href="#" onClick={(e)=> {
+                                    e.preventDefault();
+                                    dispatcher(logOut());
+                                    navigate("/");
+                                }} className="text-gray-600 hover:text-blue-500">
+                                    Вихід
+                                </a>
+                            </li>
+                        </>
+                        :
+                        <>
+                            <li>
+                                <Link to="/register" className="text-gray-600 hover:text-blue-500">
+                                    Реєстрація
+                                </Link>
+                            </li>
 
-                    <li>
-                        <Link to="/login" className="text-gray-600 hover:text-blue-500">
-                            Вхід
-                        </Link>
-                    </li>
+                            <li>
+                                <Link to="/login" className="text-gray-600 hover:text-blue-500">
+                                    Вхід
+                                </Link>
+                            </li>
+                        </>
+                    }
+
 
                 </ul>
             </nav>
